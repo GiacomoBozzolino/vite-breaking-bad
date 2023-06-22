d5b85db292560bcf1143257c30528dde461d619b
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-06-22 16:18:08 +0200

    [FEAT] creazione props per stampa in dom

--- src/components/AppCardList.vue
+++ src/components/AppCardList.vue
@@ -1,15 +1,40 @@
 <script>
+
     export default {
-    
+        // recupero la props
+    props:{
+        myPokemon:Object,
+    }
     }
 </script>
 
 <template lang="">
-    <div>
+    <div class="poke-card text-center m-3">
+        <img :src="myPokemon.imageUrl" alt="">
+        <div>{{myPokemon.number}}</div>
+        <div>{{myPokemon.name}}</div>
+        <div>{{myPokemon.type1}}</div>
+
         
     </div>
 </template>
 
-<style lang="">
+<style lang="scss" scoped>
+.poke-card{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: white;
+    border-radius: 20px;
+    img{
+
+        padding: 10px;
+        width: 190px;
+        height: 190px;
+    
+        border-radius: 10%;
+        
+    }
+}
     
 </style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,5 +1,18 @@
 <script>
+import AppCardList from '../components/AppCardList.vue';
+// importo store dal file store.js
+import {store} from '../store.js'; 
+
     export default {
+        components:{
+            AppCardList,
+            store,
+        },
+        data() {
+            return {
+                store
+            }
+        },
         
     }
 </script>
@@ -7,7 +20,12 @@
     <main>
         <div class="container" >
             <div class="card_container d-flex align-items-center justify-content-center">
-                <div class="inner_container">
+                <div class="inner_container row">
+                    <div v-for="(pokemon, index) in store.pokedex" :key="index" class="col-3">
+                        <AppCardList :myPokemon="pokemon"/>
+
+                    </div>
+                    <!-- creo una props -->
 
                 </div>
 
@@ -28,13 +46,15 @@
 
     .card_container{
         background-color: $background_white;
-        height:700px;
+        height:75vh;
         width: 100%;
         border-radius: 30px;
+        
         .inner_container{
             background-color: $background_grey;
             width: 95%;
             height: 90%;
+            overflow-y: auto;
             
         }
         
